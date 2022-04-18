72e29d0c14519099e0c7c923cddd9f46f9752edc
zygo-sem` <83294806+zygo-sem@users.noreply.github.com>
2022-04-18 11:59:15 +0900

    redux-basic

---
+++ src/reducers/actionTypes.js
@@ -0,0 +1,2 @@
+export const SELL_MEAT = 'SELL_MEAT'
+export const SELL_VEGETABLE = 'SELL_VEGETABLE'
---
+++ src/reducers/reducerVegetable.js
@@ -0,0 +1,13 @@
+import { SELL_VEGETABLE } from './actionTypes'
+
+const reducerVegetable = (state=[], action) => {
+  switch(action.type){
+    case SELL_VEGETABLE: return {
+      ...state,
+      numOfVegetable: state.numOfVegetable - 1
+    }
+    default: return state
+  }
+}
+
+export default reducerVegetable
